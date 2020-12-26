58185f880227a2afe6caf4e8c5324b9f15a5c933
Sascha Häberling <mail@sascha-haeberling.com>
2020-12-26 03:07:06 -0800

    Implement new Controller interface in Temperature controller.

--- main/temp_controller.cpp
+++ main/temp_controller.cpp
@@ -1,7 +1,12 @@
 #include "temp_controller.h"
 
+#include <string>
+#include <vector>
+
 #include "esp_log.h"
 
+#include "control_hub.h"
+#include "controller.h"
 #include "htu21d_controller.h"
 
 #ifdef __cplusplus
@@ -12,17 +17,42 @@ extern uint8_t temprature_sens_read();
 }
 #endif
 
-static const char *TAG = "temp-controller";
+static const char *TAG = "win-temp-ctrl";
 
 TempController::TempController(HTU21DController* htu21d_controller)
     : htu21d_controller_(htu21d_controller) {
 }
 
+// override
+std::vector<SensorConfig*> TempController::getSensors() const {
+  std::vector<SensorConfig*> sensors;
+  // TODO: This depends on whether another temp module was added or not.
+  for (int i = 0; i < 2; ++i) {
+    auto* c = new SensorConfig {
+      .name = "temp",
+      .idx = i,
+      .update_interval_seconds = 10,
+      .get_value = [this, i](void){ return std::to_string(this->getCelsius(i)); }
+    };
+    sensors.push_back(c);
+  }
+
+  auto* c = new SensorConfig {
+    .name = "hum",
+    .idx = 0,
+    .update_interval_seconds = 10,
+    .get_value = [this](void){ return std::to_string(this->getHumidity(0)); }
+  };
+  sensors.push_back(c);
+
+  return sensors;
+}
+
 // public 
-float TempController::getCelsius(int idx) {
+float TempController::getCelsius(int idx) const {
   // 0 is the internal temperature sensor inside the ESP32.
   if (idx == 0) {
-    // Convert F -> C.
+    // Convert F -> C. Note: very inaccurate in an absolute sense.
     return (temprature_sens_read() - 32);
   } else if (htu21d_controller_ != NULL && idx == 1) {
     return htu21d_controller_->getCelsius();
@@ -33,7 +63,7 @@ float TempController::getCelsius(int idx) {
   return -1;
 }
 
-float TempController::getHumidity(int idx) {
+float TempController::getHumidity(int idx) const {
   if (htu21d_controller_ != NULL && idx == 0) {
     return htu21d_controller_->getHumidity();
   }
--- main/temp_controller.h
+++ main/temp_controller.h
@@ -3,14 +3,22 @@
 #ifndef _WINSTON_TEMP_CONTROLLER_H_
 #define _WINSTON_TEMP_CONTROLLER_H_
 
+#include "control_hub.h"
+#include "controller.h"
 #include "htu21d_controller.h"
 
+#include <functional>
+#include <string>
+#include <vector>
+
 // Get temperature from the built-in ESP32 temp sensor.
-class TempController {
+class TempController : public Controller {
  public:
   TempController(HTU21DController* htu21d_controller);
-  float getCelsius(int idx);
-  float getHumidity(int idx);
+  std::vector<SensorConfig*> getSensors() const override;
+
+  float getCelsius(int idx) const;
+  float getHumidity(int idx) const;
 
  private:
   HTU21DController* htu21d_controller_;
