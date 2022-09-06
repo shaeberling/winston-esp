771660e676097ab4ec623aedccb24ee3566790be
Sascha Häberling <mail@sascha-haeberling.com>
2022-09-05 22:19:17 -0700

    Improved idf.sh

--- idf.sh
+++ idf.sh
@@ -5,11 +5,13 @@ if [ -z "$1" ]
     echo "1st argument missing: [serial-port number]"
     exit 1
 fi
+port="$1"
 if [ -z "$2" ]
   then
     echo "2nd argument missing: [command]"
     exit 1
 fi
+all_arguments=("$@")
+cmd_args=("${all_arguments[@]:1}")
 
-
-idf.py -p /dev/ttyS"$1" -b 921600 "$2"
+idf.py -p /dev/ttyS"$port" -b 921600 "${cmd_args[@]}"
